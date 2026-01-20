5d47983f3cb503524e96f1f17382916d80f9001a
Abu Maraqa <maraqamohamad@outlook.sa>
2026-01-20 12:33:07 +0300

    asd

--- Modules/PPUDS/Providers/SidebarServiceProvider.php
+++ Modules/PPUDS/Providers/SidebarServiceProvider.php
@@ -28,11 +28,41 @@ class SidebarServiceProvider extends ServiceProvider
         );
 
         $sidebar->add(
+            (new SidebarGroup('Specialties', 'solar-users-group-rounded-bold-duotone' , ['Student View']))
+                ->add(new SidebarItem('Add Specialties' , 'solar-users-group-rounded-bold-duotone' , ['Student View'] ,  'students.add' ))
+                ->add(new SidebarItem('Specialties List' , 'solar-users-group-rounded-bold-duotone' , ['Student Create'] ,  'students.index' ))
+        );
+
+        $sidebar->add(
+            (new SidebarGroup('Registration', 'solar-users-group-rounded-bold-duotone' , ['Student View']))
+                ->add(new SidebarItem('Add Registration' , 'solar-users-group-rounded-bold-duotone' , ['Student View'] ,  'students.add' ))
+                ->add(new SidebarItem('Registration List' , 'solar-users-group-rounded-bold-duotone' , ['Student Create'] ,  'students.index' ))
+        );
+
+        $sidebar->add(
             (new SidebarGroup('Companies', 'solar-users-group-rounded-bold-duotone' , ['Company View']))
                 ->add(new SidebarItem('Companies Category List' , 'solar-users-group-rounded-bold-duotone' , ['Company Category View'] ,  'company-category.index' ))
                 ->add(new SidebarItem('Companies Department List' , 'solar-users-group-rounded-bold-duotone' , ['Company Department View'] ,  'company-department.index' ))
                 ->add(new SidebarItem('Add Company' , 'solar-users-group-rounded-bold-duotone' , ['Company View'] ,  'companies.add' ))
                 ->add(new SidebarItem('Companies List' , 'solar-users-group-rounded-bold-duotone' , ['Company Create'] ,  'companies.index' ))
+        );
+
+        $sidebar->add(
+            (new SidebarGroup('Students of the current semester', 'solar-users-group-rounded-bold-duotone' , ['Student View']))
+                ->add(new SidebarItem('Add Students of the current semester' , 'solar-users-group-rounded-bold-duotone' , ['Student View'] ,  'students.add' ))
+                ->add(new SidebarItem('Students of the current semester List' , 'solar-users-group-rounded-bold-duotone' , ['Student Create'] ,  'students.index' ))
+        );
+
+        $sidebar->add(
+            (new SidebarItem('Attendance and departure log', 'solar-users-group-rounded-bold-duotone' , ['Student View'], 'students.index'))
+        );
+
+        $sidebar->add(
+            (new SidebarItem('Follow-up file', 'solar-users-group-rounded-bold-duotone' , ['Student View'] , 'students.index'))
+        );
+
+        $sidebar->add(
+            (new SidebarItem('Ratings', 'solar-users-group-rounded-bold-duotone' , ['Student View'] , 'students.index'))
         );
     }
 
