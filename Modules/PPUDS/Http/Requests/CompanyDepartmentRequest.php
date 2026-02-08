742c0787a651194486a4c0f257af8fe8009a53f0
Abu Maraqa <maraqamohamad@outlook.sa>
2026-02-08 14:53:38 +0300

    Fix Request For Roles In Larae

---
+++ Modules/PPUDS/Http/Requests/CompanyDepartmentRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\PPUDS\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Modules\PPUDS\Enums\CompanyStatus;
+
+class CompanyDepartmentRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'name'                  => ['required', 'string', 'max:255'],
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
