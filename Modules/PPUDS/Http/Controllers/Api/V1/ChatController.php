2a90ff02b441634dd3cf672ed3ddfcb1c107941d
Abu Maraqa <maraqamohamad@outlook.sa>
2026-02-26 16:12:44 +0300

    asd

--- Modules/PPUDS/Http/Controllers/Api/V1/ChatController.php
+++ Modules/PPUDS/Http/Controllers/Api/V1/ChatController.php
@@ -108,7 +108,7 @@ class ChatController extends Controller
 
         abort_if(auth()->id() === $receiver->id, 400, __('You cannot create a conversation with yourself.'));
 
-        $conversation = auth()- >user()->createConversationWith($receiver);
+        $conversation = auth()->user()->createConversationWith($receiver);
 
         $conversation->load(['participants', 'lastMessage']);
 
