0a038970cca393fe85ff20ce40079514317e03b0
AbuMaraqa <maraqamohamad@outlook.sa>
2026-04-09 10:43:18 +0300

    asd

--- Modules/PPUDS/Http/Requests/CompanyRequest.php
+++ Modules/PPUDS/Http/Requests/CompanyRequest.php
@@ -23,7 +23,7 @@ class CompanyRequest extends FormRequest
             // Branch Validation
             'branches'                => ['required', 'array', 'min:1'],
             'branches.*.name'         => ['required', 'string', 'max:255'],
-            'branches.*.email'        => ['nullable', 'email', 'max:255', 'distinct', 'unique:branch_table_name,email'],
+            'branches.*.email'        => ['nullable', 'email', 'max:255', 'distinct', 'unique:' . config('branch.table_prefix') . 'branches,email'],
             'branches.*.phone'        => ['nullable', 'string', 'max:50'],
             'branches.*.country_id'   => ['required', 'integer', 'exists:geolocation_countries,id'],
             'branches.*.city_id'      => ['required', 'integer', 'exists:geolocation_cities,id'],
