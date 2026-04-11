5488b4ad005bef8884181f7e2a2ba92dadd19433
AbuMaraqa <maraqamohamad@outlook.sa>
2026-04-11 16:23:46 +0300

    asd

---
+++ Modules/PPUDS/Http/Requests/StudentAttendanceRequestUpdate.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Modules\PPUDS\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Modules\PPUDS\Entities\StudentCompany;
+use Modules\PPUDS\Enums\Enums\CompanyStatus;
+
+class StudentAttendanceRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'attendance_date' => [
+                'nullable',
+                'date',
+            ],
+
+            'check_in' => [
+                'nullable',
+                'date_format:Y-m-d H:i:s',
+            ],
+
+            'check_out' => [
+                'nullable',
+                'date_format:Y-m-d H:i:s',
+                'after:check_in',
+            ],
+
+            'student_company_id' => [
+                'sometimes',
+                'integer',
+                Rule::exists(StudentCompany::class, 'id'),
+            ],
+
+            'latitude' => [
+                'sometimes',
+                'numeric',
+                'between:-90,90',
+            ],
+
+            'longitude' => [
+                'sometimes',
+                'numeric',
+                'between:-180,180',
+            ],
+
+            'description' => [
+                'nullable',
+                'string',
+                'max:500',
+            ],
+        ];
+    }
+}
