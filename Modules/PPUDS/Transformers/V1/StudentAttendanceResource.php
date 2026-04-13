61aea7d511caab195b6ac8ca19c53b7c50924e0a
AbuMaraqa <maraqamohamad@outlook.sa>
2026-04-13 14:33:12 +0300

    Add Filters For Student Attendance

--- Modules/PPUDS/Transformers/V1/StudentAttendanceResource.php
+++ Modules/PPUDS/Transformers/V1/StudentAttendanceResource.php
@@ -2,6 +2,7 @@
 
 namespace Modules\PPUDS\Transformers\V1;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Spatie\QueryBuilder\AllowedFilter;
@@ -12,29 +13,29 @@ class StudentAttendanceResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'                 => $this->id,
+            'id' => $this->id,
             'student_company_id' => $this->student_company_id,
-            'attendance_date'    => $this->attendance_date?->format('Y-m-d'),
+            'attendance_date' => $this->attendance_date?->format('Y-m-d'),
 
             'check_in' => [
-                'time'      => $this->check_in?->format('H:i:s'),
-                'latitude'  => $this->check_in_latitude,
+                'time' => $this->check_in?->format('H:i:s'),
+                'latitude' => $this->check_in_latitude,
                 'longitude' => $this->check_in_longitude,
             ],
 
             'check_out' => [
-                'time'      => $this->check_out?->format('H:i:s'),
-                'latitude'  => $this->check_out_latitude,
+                'time' => $this->check_out?->format('H:i:s'),
+                'latitude' => $this->check_out_latitude,
                 'longitude' => $this->check_out_longitude,
             ],
 
-            'status'      => $this->status,
+            'status' => $this->status,
             'description' => $this->description,
-            'created_by'  => $this->created_by,
-            'created_at'  => $this->created_at?->toIso8601String(),
+            'created_by' => $this->created_by,
+            'created_at' => $this->created_at?->toIso8601String(),
 
             'student_company' => new StudentCompanyResource($this->whenLoaded('studentCompany')),
-            'creator'         => $this->whenLoaded('createdBy'),
+            'creator' => $this->whenLoaded('createdBy'),
         ];
     }
 
@@ -57,6 +58,27 @@ class StudentAttendanceResource extends JsonResource
             AllowedFilter::exact('status'),
             AllowedFilter::scope('date_between'),
             AllowedFilter::exact('attendance_date'),
+
+            AllowedFilter::callback('student_id', function (Builder $query, $value) {
+                $query->whereHas('studentCompany.registration', function ($query) use ($value) {
+                    $query->where('student_id', $value);
+                });
+            }),
+            AllowedFilter::callback('company_id', function (Builder $query, $value) {
+                $query->whereHas('studentCompany.registration', function ($query) use ($value) {
+                    $query->where('company_id', $value);
+                });
+            }),
+            AllowedFilter::callback('semester', function (Builder $query, $value) {
+                $query->whereHas('studentCompany.registration', function ($query) use ($value) {
+                    $query->where('semester', $value);
+                });
+            }),
+            AllowedFilter::callback('year', function (Builder $query, $value) {
+                $query->whereHas('studentCompany.registration', function ($query) use ($value) {
+                    $query->where('year', $value);
+                });
+            }),
         ];
     }
 
