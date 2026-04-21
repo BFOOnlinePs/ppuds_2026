b321e98f79924c15c4fbc7bfcb799454e5526e9d
AbuMaraqa <maraqamohamad@outlook.sa>
2026-04-21 13:18:36 +0300

    Add Partner

--- Modules/PPUDS/Transformers/V1/ConversationResource.php
+++ Modules/PPUDS/Transformers/V1/ConversationResource.php
@@ -4,6 +4,7 @@ namespace Modules\PPUDS\Transformers\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Modules\Core\Transformers\V1\UserResource;
 use Spatie\QueryBuilder\AllowedSort;
 
 class ConversationResource extends JsonResource
@@ -13,30 +14,36 @@ class ConversationResource extends JsonResource
         $authId = auth()->id();
 
         return [
-            'id'                => $this->id,
-            'type'              => $this->type,
+            'id' => $this->id,
+            'type' => $this->type,
 
-            'name'              => $this->name ?: $this->getPartnerName($authId),
+            'name' => $this->name ?? ($this->type == 'private' ? $this->getRecipientName() : null),
 
-            'unread_count'      => $this->getUnreadCount($authId),
+            'partner' => $this->whenLoaded('partners', function () use ($authId) {
 
-            'last_active_at'    => $this->whenLoaded('participants', fn() =>
-                                        $this->participants->firstWhere('participantable_id', $authId)?->last_active_at
-                                   ),
+                $user = $this->participants->firstWhere('participantable_id', '!=', $authId)?->participantable;
 
-            'created_at'        => $this->created_at,
-            'updated_at'        => $this->updated_at,
+                return $user ? new UserResource($user) : null;
 
-            'last_message'      => new MessageResource($this->whenLoaded('lastMessage')),
-            'participants'      => $this->whenLoaded('participants'),
-            'group_details'     => $this->when($this->type === 'group', $this->group),
+            }),
+
+            'unread_count' => $this->getUnreadCount($authId),
+
+            'last_active_at' => $this->whenLoaded('participants', fn () => $this->participants->firstWhere('participantable_id', $authId)?->last_active_at
+            ),
+
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+
+            'last_message' => new MessageResource($this->whenLoaded('lastMessage')),
+            'participants' => $this->whenLoaded('participants'),
+            'group_details' => $this->when($this->type === 'group', $this->group),
         ];
     }
 
     private function getPartnerName(int $authId)
     {
-        return $this->whenLoaded('participants', fn() =>
-            $this->participants->firstWhere('participantable_id', '!=', $authId)?->participantable?->name
+        return $this->whenLoaded('participants', fn () => $this->participants->firstWhere('participantable_id', '!=', $authId)?->participantable?->name
         );
     }
 
@@ -44,7 +51,7 @@ class ConversationResource extends JsonResource
     {
         return (int) $this->messages()
             ->where('sendable_id', '!=', $authId)
-            ->where('created_at', '>', function($query) use ($authId) {
+            ->where('created_at', '>', function ($query) use ($authId) {
                 $query->select('conversation_read_at')
                     ->from('wirechat_participants')
                     ->where('conversation_id', $this->id)
