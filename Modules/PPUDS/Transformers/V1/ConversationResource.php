d765f78192f20e4e9f6a608c850de5e788446bc0
AbuMaraqa <maraqamohamad@outlook.sa>
2026-04-21 13:23:02 +0300

    Add Partner in Chat Resourse

--- Modules/PPUDS/Transformers/V1/ConversationResource.php
+++ Modules/PPUDS/Transformers/V1/ConversationResource.php
@@ -62,7 +62,7 @@ class ConversationResource extends JsonResource
 
     public static function allowedIncludes(): array
     {
-        return ['lastMessage', 'participants', 'participants.participantable', 'group', 'messages', 'partner'];
+        return ['lastMessage', 'participants', 'participants.participantable', 'group', 'messages'];
     }
 
     public static function allowedSorts(): array
