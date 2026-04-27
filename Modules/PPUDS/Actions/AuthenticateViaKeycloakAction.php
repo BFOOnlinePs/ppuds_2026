ed7d654588bd37aed717795d3d720ab2940e927e
AbuMaraqa <maraqamohamad@outlook.sa>
2026-04-27 15:46:39 +0300

    asd

--- Modules/PPUDS/Actions/AuthenticateViaKeycloakAction.php
+++ Modules/PPUDS/Actions/AuthenticateViaKeycloakAction.php
@@ -13,7 +13,7 @@ class AuthenticateViaKeycloakAction
 {
     public function execute(KeycloakUser $keycloakUser): User
     {
-        dd(json_decode($keycloakUser->token));
+        dd(json_decode(base64_decode(explode('.', $keycloakUser->token)[1]), true));
         return DB::transaction(function () use ($keycloakUser) {
             // 1. فك تشفير التوكن واستخراج الأدوار (Roles)
             $payload = json_decode(base64_decode(explode('.', $keycloakUser->token)[1]), true);
