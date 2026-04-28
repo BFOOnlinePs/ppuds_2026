a082a37d0283da1092ae42b9d421834f8db41a3d
AbuMaraqa <maraqamohamad@outlook.sa>
2026-04-28 18:32:53 +0300

    asd

--- Modules/PPUDS/Providers/PPUDSServiceProvider.php
+++ Modules/PPUDS/Providers/PPUDSServiceProvider.php
@@ -40,6 +40,14 @@ class PPUDSServiceProvider extends ServiceProvider
             SocialiteWasCalled::class,
             [KeycloakExtendSocialite::class, 'handle']
         );
+
+        $this->app->bind(EloquentUserProvider::class, function ($app, array $config) {
+        return new class($app['hash'], $config['model']) extends EloquentUserProvider {
+            public function retrieveByToken($token, $credentials) {
+                return (new KeycloakUserRepository())->retrieveByToken($token, $credentials);
+            }
+        };
+    });
     }
 
     /**
