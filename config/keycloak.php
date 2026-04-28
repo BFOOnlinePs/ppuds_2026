26ecdc6ced4c9b2eebdb083d6f206b5b00ff0688
AbuMaraqa <maraqamohamad@outlook.sa>
2026-04-28 18:29:22 +0300

    asd

--- config/keycloak.php
+++ config/keycloak.php
@@ -1,5 +1,7 @@
 <?php
 
+use Modules\PPUDS\Services\KeycloakUserRepository;
+
 return [
     'realm_public_key' => env('KEYCLOAK_REALM_PUBLIC_KEY', null),
 
@@ -7,7 +9,10 @@ return [
 
     'load_user_from_database' => env('KEYCLOAK_LOAD_USER_FROM_DATABASE', true),
 
-    'user_provider_custom_retrieve_method' => 'retrieveByToken',
+    'user_provider_custom_retrieve_method' => function($token, $credentials) {
+        return (new KeycloakUserRepository())->retrieveByToken($token, $credentials);
+    },
+
     'user_provider_credential' => env('KEYCLOAK_USER_PROVIDER_CREDENTIAL', 'email'),
 
     'token_principal_attribute' => env('KEYCLOAK_TOKEN_PRINCIPAL_ATTRIBUTE', 'email'),
