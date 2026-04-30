17ba12144c76dc5018ab2bc75ddb154cb3ea3847
AbuMaraqa <maraqamohamad@outlook.sa>
2026-04-30 13:02:50 +0300

    asd

--- Modules/Core/database/migrations/2026_04_30_095241_create_app_version_table.php
+++ Modules/Core/database/migrations/2026_04_30_095241_create_app_version_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {s
+    {
         Schema::create('app_version', function (Blueprint $table) {
             $table->id();
 
