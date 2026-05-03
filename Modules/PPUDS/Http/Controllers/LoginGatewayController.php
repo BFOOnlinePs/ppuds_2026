d5eea9dc79b411336ec4bb781661a148dbafbd55
AbuMaraqa <maraqamohamad@outlook.sa>
2026-05-03 15:52:13 +0300

    asd

--- Modules/PPUDS/Http/Controllers/LoginGatewayController.php
+++ Modules/PPUDS/Http/Controllers/LoginGatewayController.php
@@ -11,7 +11,7 @@ class LoginGatewayController extends Controller
     public function __invoke()
     {
         $generalSettings = app(GeneralSettings::class);
-        dd($generalSettings->login_method);
+        dd($generalSettings->login_method === \Modules\PPUDS\Enums\LoginMethod::PPU->value);
         if ($generalSettings->login_method === \Modules\PPUDS\Enums\LoginMethod::PPU->value) {
             return redirect()->route('keycloak.redirect');
         }
