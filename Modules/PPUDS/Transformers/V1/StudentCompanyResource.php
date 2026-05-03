01673ca062b03f9e2edf46d5613ce61bfd33e449
AbuMaraqa <maraqamohamad@outlook.sa>
2026-05-03 13:43:12 +0300

    Add Semester And Year In APi

--- Modules/PPUDS/Transformers/V1/StudentCompanyResource.php
+++ Modules/PPUDS/Transformers/V1/StudentCompanyResource.php
@@ -95,6 +95,18 @@ class StudentCompanyResource extends JsonResource
                         });
                 });
             }),
+
+            AllowedFilter::callback('semester', function (Builder $query, $value) {
+                $query->whereHas('registration', function (Builder $registrationQuery) use ($value) {
+                    $registrationQuery->where('semester_id', $value);
+                });
+            }),
+
+            AllowedFilter::callback('year', function (Builder $query, $value) {
+                $query->whereHas('registration', function (Builder $registrationQuery) use ($value) {
+                    $registrationQuery->where('year_id', $value);
+                });
+            }),
         ];
     }
 
