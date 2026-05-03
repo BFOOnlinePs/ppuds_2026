35e761bbeec89c2fa3532bb88226cbd44ade96f9
AbuMaraqa <maraqamohamad@outlook.sa>
2026-05-03 16:26:19 +0300

    asd

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Modules\Core\Traits\ApiResponse;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,5 +26,20 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->render(function (\KeycloakGuard\Exceptions\TokenException $e, \Illuminate\Http\Request $request) {
+            if ($request->expectsJson() || $request->is('api/*')) {
 
+                // إنشاء كلاس مجهول لاستخدام الـ Trait
+                $responder = new class {
+                    use \Modules\Core\Traits\ApiResponse;
+
+                    // دالة عامة للوصول للدالة المحمية داخل الـ Trait
+                    public function sendError($message, $code) {
+                        return $this->errorResponse($message, $code);
+                    }
+                };
+
+                return $responder->sendError('Unauthenticated.', 401);
+            }
+        });
     })->create();
